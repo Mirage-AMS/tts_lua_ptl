75fa3e351ddbb651486283b57f8f7cc12b85725d
IricAnod <IricWei@126.com>
2025-07-07 18:41:44 +0800

    2025/07/07 1. add a test realization for dynamic register

--- src/event_dev_mode.lua
+++ src/event_dev_mode.lua
@@ -1,4 +1,62 @@
 require("com/enum_const")
+require("com/deck_const")
+require("com/object_type")
+
+--- registerDeck: generate all deck items and register info on them
+--- @param deck any: The deck to register.
+--- @param info table|nil: The information about the deck.
+--- @return nil
+local function registerDeck(deck, info)
+    -- quick break if not legal deck object
+    if deck == nil then
+        error("fatal error: a nil deck object was passed to register")
+    end
+    if not isCardLike(deck) then
+        error("fatal error: a non-card object was passed to register")
+    end
+    local cardNum = numCard(deck)
+    if info and #info ~= cardNum then
+        error("fatal error: info table length does not match card number")
+    end
+
+    -- register the deck
+    deck.setLock(true)
+    local cardSet = {}
+    local _initShift = 1.75
+    local _eachShift = 0.2
+    local pos = deck.getPosition()
+    pos.y = pos.y + _initShift
+
+    -- closure to create a callback function for each card
+    local function createCallback(cardInfo)
+        return function(spawnedObject)
+            table.insert(cardSet, spawnedObject)
+            spawnedObject.setLock(true)
+            if cardInfo then
+                spawnedObject.name = cardInfo.name
+                spawnedObject.memo = cardInfo.memo
+            end
+        end
+    end
+
+    -- 生成卡牌
+    for idx = cardNum, 1, -1 do
+        local cardInfo = info and info[idx]  -- 提前获取当前卡牌信息
+        deck.takeObject({
+            index = idx,
+            position = pos,
+            callback_function = createCallback(cardInfo)  -- 传递信息而非索引
+        })
+        pos.y = pos.y + _eachShift
+    end
+
+    Wait.time(function()
+        for _, eacCard in cardSet do
+            eacCard.setLock(false)
+        end
+    end,
+    2)
+end
 
 --- initDevelopmentMode: Initializes the game in development mode.
 --- @return nil
@@ -14,6 +72,33 @@ end
 local function cleanDevelopmentMode()
     -- TODO implement development mode cleanup
     broadcastToAll("Development Mode Disabled")
+
+    -- ONLY FOR FUNCTION TESTING
+    local info = {}
+    local CO_STD_MAPPING = {
+        [1] = { prefix = "CO_STD01_", data = CO_STD_01 },
+        [2] = { prefix = "CO_STD02_", data = CO_STD_02 }
+    }
+
+    -- 循环生成信息
+    for idx = 1, 60 do
+        local idxMapped = ((idx - 1) % 30) + 1
+        local group = math.floor((idx - 1) / 30) + 1
+        local config = CO_STD_MAPPING[group]
+        if not config then
+            error("fatal error: no config found for group " .. tostring(group))
+        end
+        table.insert(info, {
+            name = config.data[idxMapped],
+            memo = config.prefix .. string.format("%02d", idxMapped)
+        })
+    end
+
+    local publicItemManager = GAME:getPublicItemManager()
+    local coventicleZone = publicItemManager:getZone(NAME_ZONE_CONVENTICLE)
+    local conventicleDeck = coventicleZone:getDeckObj()
+    registerDeck(conventicleDeck, info)
+
     GAME.public_service:setMode(GameMode.Guest)
 end
 
