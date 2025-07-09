028835f3eccc5fdebfade0b99fb650a54b8ad291
IricAnod <IricWei@126.com>
2025-07-09 16:49:15 +0800

    2025/07/09 1. write data in dev_const.lua

--- com/dev_const.lua
+++ com/dev_const.lua
@@ -1,3 +1,4 @@
+require("mock/default")
 require("com/const")
 
 -- DECKS CONST
@@ -126,6 +127,12 @@ NAME_BOARD_ROLE_DISPLAY_01 = "board_role_display_01"
 NAME_BOARD_ROLE_DISPLAY_02 = "board_role_display_02"
 NAME_BOARD_ROLE_DISPLAY_03 = "board_role_display_03"
 NAME_BOARD_ROLE_DISPLAY_04 = "board_role_display_04"
+BOARD_ROLE_DISPLAY_LIST = {
+    [1] = NAME_BOARD_ROLE_DISPLAY_01,
+    [2] = NAME_BOARD_ROLE_DISPLAY_02,
+    [3] = NAME_BOARD_ROLE_DISPLAY_03,
+    [4] = NAME_BOARD_ROLE_DISPLAY_04,
+}
 
 NAME_ZONE_DEVELOPMENT = "zone_development"
 NAME_ZONE_ROLE_PICK = "zone_role_pick"
@@ -175,6 +182,92 @@ LIST_PARAM_SCRIPTING_DEV_MODE = {
             z_num=6, z_shift={x=0,y=0,z=-5.60},
         },
     }
+}
+
+-- ref to certain role display board
+LIST_PARAM_ROLE_DISPLAY = {
+    [PREFIX_RO_INT01] = {
+        [1] = {id = 1, idx = 1, idz = 3},  -- 达维安，巨人杀手
+        [2] = {id = 1, idx = 2, idz = 1},  -- 盖尔曼，历战勇士
+        [3] = {id = 1, idx = 2, idz = 2},  -- 埃尔，铁与火
+        [4] = {id = 1, idx = 1, idz = 2},  -- 泽丽尔，树屋精灵
+        [5] = {id = 2, idx = 1, idz = 2},  -- 保罗与波娜，那对兄妹
+        [6] = {id = 2, idx = 2, idz = 1},  -- 凯瑟琳，完美主义
+        [7] = {id = 1, idx = 2, idz = 3},  -- 希玛，狩魔不息
+        [8] = {id = 2, idx = 2, idz = 3},  -- 塔尔凡，漫游吟者
+        [9] = {id = 1, idx = 1, idz = 1},  -- 诺维，初生牛犊
+        [10] = {id = 2, idx = 2, idz = 2},  -- 夏洛克，独断专营
+        [11] = {id = 2, idx = 1, idz = 1},  -- 索兰，高山行者
+        [12] = {id = 2, idx = 1, idz = 3},  -- 菲莉斯，圣女垂怜
+    },
+    [PREFIX_RO_INT02] = {
+        [1] = {id = 3, idx = 1, idz = 1},  -- 法师，王朝余烬
+        [2] = {id = 3, idx = 1, idz = 2},  -- 莱奥，命运之剑
+        [3] = {id = 3, idx = 2, idz = 1},  -- 艾尔弗里德，大展宏图
+        [4] = {id = 3, idx = 2, idz = 2},  -- 莱拉娜，丰饶祭祀
+        [5] = {id = 3, idx = 1, idz = 3},  -- 欧也妮，匠心独运
+        [6] = {id = 3, idx = 2, idz = 3},  -- 弗林特，栖身林原
+        [7] = {id = 4, idx = 1, idz = 1},  -- 艾丽娅，驯鹰人
+        [8] = {id = 4, idx = 2, idz = 1},  -- 卡洛斯，万咒环身
+        [9] = {id = 4, idx = 1, idz = 2},  -- 凯，嗜赌成性
+    },
+    [PREFIX_RO_INF01] = {
+        [1] = {id = 1, idx = 1, idz = 3, idxx = 2, idzz = 1},  -- 嗜血药剂
+        [2] = {id = 1, idx = 1, idz = 3, idxx = 3, idzz = 1},  -- 达维恩的巨人杀手
+        [3] = {id = 1, idx = 2, idz = 1, idxx = 2, idzz = 1},  -- 卓越战技：诱敌深入
+        [4] = {id = 1, idx = 2, idz = 1, idxx = 3, idzz = 1},  -- 卓越战绩：决胜一击
+        [5] = {id = 1, idx = 2, idz = 2, idxx = 2, idzz = 1},  -- 金刚铸魂
+        [6] = {id = 1, idx = 2, idz = 2, idxx = 3, idzz = 1},  -- 激昂澎湃
+        [7] = {id = 1, idx = 1, idz = 2, idxx = 2, idzz = 1},  -- 活力药剂
+        [8] = {id = 1, idx = 1, idz = 2, idxx = 3, idzz = 1},  -- 润物无声
+        [9] = {id = 2, idx = 1, idz = 2, idxx = 2, idzz = 1},  -- 波娜的记账簿
+        [10] = {id = 2, idx = 1, idz = 2, idxx = 3, idzz = 1},  -- 冰火轮舞
+        [11] = {id = 2, idx = 2, idz = 1, idxx = 3, idzz = 1},  -- 优雅之舞
+        [12] = {id = 2, idx = 2, idz = 1, idxx = 2, idzz = 1},  -- 风之华舞
+        [13] = {id = 1, idx = 2, idz = 3, idxx = 2, idzz = 1},  -- 弑魔者
+        [14] = {id = 1, idx = 2, idz = 3, idxx = 3, idzz = 1},  -- 万象法咒
+        [15] = {id = 2, idx = 2, idz = 3, idxx = 2, idzz = 1},  -- 漫游灵药
+        [16] = {id = 2, idx = 2, idz = 3, idxx = 3, idzz = 1},  -- 冒险奇谭
+        [17] = {id = 1, idx = 1, idz = 1, idxx = 2, idzz = 1},  -- 冒险启程
+        [18] = {id = 1, idx = 1, idz = 1, idxx = 3, idzz = 1},  -- 勤能补拙
+        [19] = {id = 2, idx = 2, idz = 2, idxx = 2, idzz = 1},  -- 货品退换
+        [20] = {id = 2, idx = 2, idz = 2, idxx = 3, idzz = 1},  -- 垄断经营
+        [21] = {id = 2, idx = 1, idz = 1, idxx = 2, idzz = 1},  -- 爆破专家
+        [22] = {id = 2, idx = 1, idz = 1, idxx = 3, idzz = 1},  -- 山脊眺望
+        [23] = {id = 2, idx = 1, idz = 3, idxx = 2, idzz = 1},  -- 圣泉赐福
+        [24] = {id = 2, idx = 1, idz = 3, idxx = 3, idzz = 1},  -- 圣女祝祷
+    },
+    [PREFIX_RO_INF02] = {
+        [1]  = {id = 3, idx = 1, idz = 1, idxx = 2, idzz = 1},  -- 魔咒闪回
+        [2]  = {id = 3, idx = 1, idz = 1, idxx = 3, idzz = 1},  -- 瞬灭
+        [3]  = {id = 3, idx = 1, idz = 2, idxx = 2, idzz = 1},  -- 试剑真知
+        [4]  = {id = 3, idx = 1, idz = 2, idxx = 3, idzz = 1, idxxx = 1, idzzz = 1},  -- 剑心·浪游
+        [5]  = {id = 3, idx = 1, idz = 2, idxx = 3, idzz = 1, idxxx = 2, idzzz = 1},  -- 剑心·勇武
+        [6]  = {id = 3, idx = 1, idz = 2, idxx = 3, idzz = 1, idxxx = 3, idzzz = 1},  -- 剑心·统领
+        [7]  = {id = 3, idx = 2, idz = 1, idxx = 2, idzz = 1},  -- 执政手腕
+        [8]  = {id = 3, idx = 2, idz = 1, idxx = 3, idzz = 1},  -- 令行禁止
+        [9]  = {id = 3, idx = 2, idz = 2, idxx = 2, idzz = 1},  -- 丰饶祈愿
+        [10] = {id = 3, idx = 2, idz = 2, idxx = 3, idzz = 1},  -- 虔敬花冠
+        [11] = {id = 3, idx = 1, idz = 3, idxx = 2, idzz = 1},  -- 独具匠心
+        [12] = {id = 3, idx = 1, idz = 3, idxx = 3, idzz = 1},  -- 勒菲弗巧械铺
+        [13] = {id = 3, idx = 2, idz = 3, idxx = 2, idzz = 1},  -- 林地作业
+        [14] = {id = 3, idx = 2, idz = 3, idxx = 3, idzz = 1},  -- 林中小屋
+        [15] = {id = 4, idx = 1, idz = 1, idxx = 2, idzz = 1},  -- 鹰舍邂逅
+        [16] = {id = 4, idx = 2, idz = 1, idxx = 2, idzz = 1},  -- 万咒法典
+        [17] = {id = 4, idx = 2, idz = 1, idxx = 3, idzz = 1},  -- 法术研习
+        [18] = {id = 4, idx = 1, idz = 2, idxx = 2, idzz = 1},  -- 老千惯犯
+        [19] = {id = 4, idx = 1, idz = 2, idxx = 3, idzz = 1},  -- 连战连胜
+    },
+}
+
+ROLE_DISPLAY_BOARD_PATTERN = {
+    origin = Vector(-11.84, 0.20, -7.38),
+    dx =  15.11,
+    dz = -7.38,
+    dxx = 4.3,
+    dzz = 0.0,
+    dxxx = 1.0,
+    dzzz = 0.0,
 }
 
 -- VAR
