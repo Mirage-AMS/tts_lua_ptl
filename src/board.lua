fbfc592fb967f6f22cbda49eb5bc359cdc44aaf1
IricAnod <IricWei@126.com>
2025-07-09 16:56:02 +0800

    2025/07/09 1. fix board methods

--- src/board.lua
+++ src/board.lua
@@ -4,29 +4,34 @@ function FactoryCreateBoard()
         object = nil
     }
 
+    -- get Method
+    function board:getPosition()
+        if not self.object then
+            error("fatal error: Board object is nil")
+        end
+        return self.object.getPosition()
+    end
+
     --------------------------------------------------------
     -- Button Methods
     --------------------------------------------------------
     function board:createButton(param)
         if not self.object then
-            print("Board not found")
-            return nil
+            error("fatal error: Board object is nil")
         end
         self.object.createButton(param)
     end
 
     function board:getButtons()
         if not self.object then
-            print("Board not found")
-            return nil
+            error("fatal error: Board object is nil")
         end
         return self.object.getButtons()
     end
 
     function board:editButton(param)
         if not self.object then
-            print("Board not found")
-            return
+            error("fatal error: Board object is nil")
         end
         self.object.editButton(param)
     end
@@ -36,8 +41,7 @@ function FactoryCreateBoard()
     -- --------------------------------------------------
     function board:getValueByIndex(index)
         if not self.object then
-            print("Board not found")
-            return
+            error("fatal error: Board object is nil")
         end
         local input = (self.object.getInputs() or {})[index]
         return input and tonumber(input["value"]) or nil
@@ -45,8 +49,7 @@ function FactoryCreateBoard()
 
     function board:tiltValueByIndex(index, value)
         if not self.object then
-            print("Board not found")
-            return
+            error("fatal error: Board object is nil")
         end
 
         if value == 0 then return end
@@ -68,6 +71,11 @@ function FactoryCreateBoard()
     function board:onLoad(data)
         self.guid = data.guid
         self.object = getObjectFromGUID(data.guid)
+
+        if not self.object then
+            error("fatal error: Board with guid" .. data.guid .. " not found")
+        end
+
         return self
     end
 
