c61971d441a19fad4de7d4f159a7d9cb349d96c1
IricAnod <IricWei@126.com>
2025-07-10 14:42:55 +0800

    2025/07/10 1. add __call to enum 2. add more enum

--- com/enum.lua
+++ com/enum.lua
@@ -1,9 +1,18 @@
 function Enum(t)
     local proxy = {}
+    local values = {}
+
+    for _, v in pairs(t) do
+        values[v] = true
+    end
+
     local mt = {
         __index = t,
         __newindex = function()
             error("Enum is read-only", 2)
+        end,
+        __call = function(_, value)
+            return values[value] ~= nil
         end
     }
     setmetatable(proxy, mt)
--- com/enum_const.lua
+++ com/enum_const.lua
@@ -3,9 +3,24 @@ require("com/enum")
 ---@class DevMode
 ---@field Development number
 ---@field Guest number
-DevMode = Enum(
-    {
-        Development = 1,
-        Guest = 2,
-    }
-)
+---@operator call(number):boolean
+DevMode = Enum({ Development = 1, Guest = 2, })
+
+---@class EnumDeckSet
+---@field STD number
+---@field DLC01 number
+---@field DLC02 number
+---@operator call(number):boolean
+EnumDeckSet = Enum({ STD = 1, DLC01 = 2, DLC02 = 3,})
+
+---@class EnumGameGoal
+---@field QUICK number
+---@field STANDARD number
+---@operator call(number):boolean
+EnumGameGoal = Enum({QUICK = 1, STANDARD = 2,})
+
+---@class EnumBPStrategy
+---@field STANDARD number
+---@field RANDOM number
+---@operator call(number):boolean
+EnumBPStrategy = Enum({STANDARD = 1, RANDOM = 2,})
