bfdf14e6a651b42597ba62d2fb1064c808886251
IricAnod <IricWei@126.com>
2025-08-14 14:49:00 +0800

    2025/08/14 1. onPlayerTurn支持GameEnd检查

--- main.lua
+++ main.lua
@@ -20,46 +20,82 @@ onPlayerTurn(player, previous_player)
 ]]--
 ---@diagnostic disable-next-line:lowercase-global
 function onPlayerTurn(player, _)
+    --- upload data to server
+    local function __uploadData()
+        ---@TODO: WebRequest not implemented yet
+        Wait.frames(
+            function()
+                print("Game onSnapshot")
+                GAME:onSnapshot()
+            end,
+        1
+        )
+    end
+
+    --- clear all discard zones
+    local function __cleanupDiscards()
+        for _, each_player_color in ipairs(DEFAULT_PLAYER_COLOR_LIST) do
+            clearPlayerDiscardZone(each_player_color)
+        end
+    end
+
     -- state update
     local turnManager = GAME:getTurnManager()
+
+    --------------------------------------------------------------------
+    -- Quick Break
+    --------------------------------------------------------------------
+    if turnManager:isGameEnd() then
+        broadcastToAll("游戏已经结束", DEFAULT_COLOR_WHITE)
+        return
+    end
+
     if not player then
         broadcastToAll("当前回合玩家为空, Turn被关闭或没有落座玩家", DEFAULT_COLOR_RED)
         return
     end
 
+    --------------------------------------------------------------------
+    -- Abnormal Turn Begginning
+    --------------------------------------------------------------------
+
     local player_color = player.color
-    if turnManager:getFirstPlayer() == nil then
-        broadcastToAll("[热座模式]未经由开局按钮开始, 如非预期请联系开发者.")
+    if turnManager:getFirstPlayer() == nil and turnManager:getRound() == 0 then
+        broadcastToAll("[热座模式]未经由开局按钮开始, 如非预期请联系开发者.", DEFAULT_COLOR_RED)
         turnManager:setFirstPlayer(player_color)
     end
 
-    --- record current player
+    --------------------------------------------------------------------
+    -- Normal Turn Begginning
+    --------------------------------------------------------------------
+
+    -- Current Player Update
     turnManager:setCurrentPlayer(player_color)
 
-    --- turn update
-    turnManager:setState(2)
-    updateButtonState()
-
-    --- round update
     if player_color == turnManager:getFirstPlayer() then
+        -- Round Update
         turnManager:addRound()
+
+        -- End Game Check (relied on turnManager.round)
+        if turnManager:isGameEnd() then
+            broadcastToAll("游戏已经结束", DEFAULT_COLOR_WHITE)
+            -- Cleanup Discards and Upload Data
+            __cleanupDiscards()
+            __uploadData()
+            return
+        end
+
         broadcastToAll("第" .. turnManager:getRound() .. "轮开始", DEFAULT_COLOR_WHITE)
     end
 
-    --- clear all discard zones
-    for _, each_player_color in ipairs(DEFAULT_PLAYER_COLOR_LIST) do
-        clearPlayerDiscardZone(each_player_color)
-    end
+    -- Turn Update
+    local TURN_STATE_BEGINNING_PHASE = 2
+    turnManager:setState(TURN_STATE_BEGINNING_PHASE)
+    updateButtonState()
 
-    --- onSnapshot
-    ---@TODO: WebRequest not implemented yet
-    Wait.frames(
-        function()
-            print("Game onSnapshot")
-            GAME:onSnapshot()
-        end,
-    1
-    )
+    -- Cleanup Discards and Upload Data
+    __cleanupDiscards()
+    __uploadData()
 end
 
 ---@diagnostic disable-next-line:lowercase-global
