974b31e4fe4ea1aaf6a2fc45b3808203d289517d
IricAnod <IricWei@126.com>
2025-11-03 08:40:29 +0800

    2025/11/03 1. 脚本同步更新路易

--- com/const_display_board.lua
+++ com/const_display_board.lua
@@ -177,6 +177,7 @@ KWORD_ROLE_KAY = "role_kay" ---- 凯，嗜赌成性
 KWORD_ROLE_COLIN = "role_colin" ---- 科林，石拳
 KWORD_ROLE_FERESHTEH = "role_fereshteh"  ---- 芙蕾什塔，丝路行商
 KWORD_ROLE_KASIM = "role_kasim"  ---- 卡西姆，祸福一念
+KWORD_ROLE_LOUIS = "role_louis"  ---- “庸王”路易
 KWORD_ROLE_NEXT = "role_next" ---- 下一角色，用于测试
 
 
@@ -448,5 +449,15 @@ ROLE_REGISTER_DICT = {
             [2] = {origin = EnumItemOrigin.DEV_DECK, prefix = PREFIX_RO_INF02, index = 24, loc_idx = 2},
             [3] = {origin = EnumItemOrigin.DEV_DECK, prefix = PREFIX_RO_INF02, index = 25, loc_idx = 3},
         }
-    }
+    },
+    [KWORD_ROLE_LOUIS] = {
+        [KWORD_ORDER]       = 25,
+        [KWORD_DIFFICULTY]  = EnumRoleDifficulty.HARD,
+        [KWORD_PREFERENCE]  = EnumRolePreference.NO_PREFERENCE,
+        [KWORD_NICKNAME]    = {"路易", "庸王", "国王"},
+        [KWORD_ITEM]        = {
+            [1] = {origin = EnumItemOrigin.DEV_DECK, prefix = PREFIX_RO_INT02, index = 13, loc_idx = 1},
+            [2] = {origin = EnumItemOrigin.DEV_DECK, prefix = PREFIX_RO_INF02, index = 26, loc_idx = 2},
+        }
+    },
 }
